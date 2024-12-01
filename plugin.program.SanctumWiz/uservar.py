b81136287bf223c630bd81bd590029996de7c084
philosophics <philosophicallyintrigued.dl@gmail.com>
2024-12-01 07:05:06 -0700

    Add files via upload

---
+++ plugin.program.SanctumWiz/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://philosophics.github.io/Sanctum/plugin.program.SanctumWiz/builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://philosophics.github.io/Sanctum/plugin.program.SanctumWiz/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
