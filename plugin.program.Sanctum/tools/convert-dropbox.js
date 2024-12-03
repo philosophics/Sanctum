de8dca7d138c4ae7d845d2b2bf4bd7c620a54491
philosophics <philosophicallyintrigued.dl@gmail.com>
2024-12-02 18:46:01 -0700

    Update convert-dropbox.js

--- plugin.program.Sanctum/tools/convert-dropbox.js
+++ plugin.program.Sanctum/tools/convert-dropbox.js
@@ -5,19 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dropbox Link Converter</title>
     <script>
-        // Function to convert the Dropbox link
         function convertDropboxLink() {
-            // Get the link from the input field
             let link = document.getElementById('dropboxLink').value;
 
-            // Replace '&' with '&amp;'
             link = link.replace(/&/g, '&amp;');
 
-            // Replace 'dl=0' with 'dl=1' (to force download)
             link = link.replace(/dl=0/, 'dl=1');
 
-            // Display the converted link in the result field
-            document.getElementById('dropboxOutput').innerText = `Converted Link: ${convertedLink}`;
+            document.getElementById('dropboxoutput').innerText = `Converted Link: ${link}`;
         }
     </script>
 </head>
@@ -29,6 +24,6 @@
     <button onclick="convertDropboxLink()">Convert Link</button>
 
     <h2>Converted Link:</h2>
-    <textarea id="result" rows="4" cols="50" readonly></textarea>
+    <p id="dropboxoutput"></p>
 </body>
 </html>
