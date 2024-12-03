9ccf4daeac5ad05849f259180dd5db94d34609ab
philosophics <philosophicallyintrigued.dl@gmail.com>
2024-12-02 18:54:36 -0700

    Update convert-dropbox.js

--- plugin.program.Sanctum/tools/convert-dropbox.js
+++ plugin.program.Sanctum/tools/convert-dropbox.js
@@ -6,13 +6,18 @@
     <title>Dropbox Link Converter</title>
     <script>
         function convertDropboxLink() {
+            console.log("Function called");
             let link = document.getElementById('dropboxLink').value;
+            console.log("Original Link:", link);
 
             link = link.replace(/&/g, '&amp;');
+            console.log("After replacing '&':", link);
 
             link = link.replace(/dl=0/, 'dl=1');
+            console.log("After replacing 'dl=0':", link);
 
             document.getElementById('dropboxoutput').innerText = `Converted Link: ${link}`;
+            console.log("Output updated");
         }
     </script>
 </head>
